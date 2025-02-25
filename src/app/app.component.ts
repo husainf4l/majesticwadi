369445e13643063aaf1be54352e73c120cddbc35
alhussein abdullah <husain.f4l@gmail.com>
2025-02-26 02:30:47 +0300

    as

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ComingsoonComponent } from "./comingsoon/comingsoon.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ComingsoonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
